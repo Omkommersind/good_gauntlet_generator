5c88a63ac6c63fb45a0542ced0db84a71ecf71eb
omkommersind <artem.chauzov@appcraft.team>
2023-01-16 23:24:46 +0300

    Registration start; bootstrap5 added.

--- landing/urls.py
+++ landing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from landing.views import index, logout
+from landing.views import index, logout_user, register_user
 
 urlpatterns = [
     path('', index, name='index'),
+    path('accounts/register', register_user, name="register")
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
 from django.contrib.auth import logout as auth_logout
+from django.shortcuts import render, redirect
+from landing.forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 
 def index(request):
     return render(request, 'landing/index.html')
 
 
-def logout(request):
+def logout_user(request):
     auth_logout(request)
-    return render(request, 'landing/index.html')
+    return redirect('index')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect('index')
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="registration/register.html", context={"register_form": form})
