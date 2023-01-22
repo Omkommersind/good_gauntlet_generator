bfa193cca862f28927af26e949c6918773b5df32
omkommersind <artem.chauzov@appcraft.team>
2023-01-22 22:25:51 +0300

    Migrations recreate

--- gauntlets/migrations/0002_initial.py
+++ gauntlets/migrations/0002_initial.py
@@ -10,29 +10,19 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('games', '0001_initial'),
+        ('gauntlets', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='lengthfeedbackmodel',
+            model_name='gauntletparticipantmodel',
             name='user',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
-            model_name='gamemodel',
-            name='platform',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='games.platformmodel'),
-        ),
-        migrations.AddField(
-            model_name='difficultyfeedbackmodel',
-            name='game',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='games.gamemodel'),
-        ),
-        migrations.AddField(
-            model_name='difficultyfeedbackmodel',
-            name='user',
+            model_name='gauntletmodel',
+            name='creator',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
